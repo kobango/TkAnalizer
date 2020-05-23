e46f2d11aef3e497813e18b01027e7ee102f95e5
kobango <kamilszkaradnik@gmail.com>
2020-05-23 23:33:20 +0200

    Reading images work!

--- Neural.py
+++ Neural.py
@@ -3,16 +3,33 @@ import numpy as np
 from nibabel.testing import data_path
 import nibabel as nib
 from matplotlib import pyplot as plt
+import med2image
+import tensorflow
 
 
 example_filename = os.path.join(data_path, 'D:\\Task01_BrainTumour\\imagesTr\\BRATS_001.nii.gz')
 img = nib.load(example_filename)
 print(img.shape)
 obraz = img.get_fdata()
+first_vol = obraz[:, :, :, 0]
+test = first_vol[:,:,70]
 
 result = obraz[:,:,140,:]
 
 
-print(result)
-plt.imshow(result, interpolation='nearest',  cmap='gray', vmin=0, vmax=255)
+print(test)
+fig = plt.figure(1)
+min = np.min(test)
+max = np.max(test)
+plt.imshow(test, interpolation='nearest',  cmap='gray', vmin=min, vmax=max)
+
+
+example_filename = os.path.join(data_path, 'D:\\Task01_BrainTumour\\labelsTr\\BRATS_001.nii.gz')
+img = nib.load(example_filename)
+obraz = img.get_fdata()
+test = obraz[:,:,70]
+
+print(test)
+fig2 = plt.figure(2)
+plt.imshow(test)
 plt.show()
